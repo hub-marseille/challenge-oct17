4304eeb40024c837dcba18b38c6b2d076bc696f6
belugu_m <belugu_m@epitech.eu>
2017-10-31 14:52:07 +0100

    Ajout affichage de texte

--- exemples/pygameExemple.py
+++ exemples/pygameExemple.py
@@ -8,15 +8,18 @@ def main():
     pygame.init()
     pygame.font.init()
     window = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+    font = pygame.font.SysFont('Arial', 30)
     image = pygame.image.load("../ressources/schema_bat2.png").convert()
     image = pygame.transform.scale(image, (720, 720))
     carre_rouge = pygame.image.load("../ressources/carre_rouge.png").convert()
     carre_rouge = pygame.transform.scale(carre_rouge, (340, 350))
     carre_vert = pygame.image.load("../ressources/carre_vert.png").convert()
     carre_vert = pygame.transform.scale(carre_vert, (320, 350))
+    text = font.render('Salle Rétro', False, (0, 0, 0))
     window.blit(image, (0, 0))
     window.blit(carre_rouge, (35, 30))
     window.blit(carre_vert, (380, 30))
+    window.blit(text, (55, 150))
     pygame.display.flip()
     on = True
     while on:
